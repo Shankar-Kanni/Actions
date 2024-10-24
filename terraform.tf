2a50224534c7f6987d58cc12e50da4aec5b4c8a3
Shankar-Kanni <87437598+Shankar-Kanni@users.noreply.github.com>
2024-10-24 11:01:03 +0200

    Added terraform file and the code

---
+++ terraform.tf
@@ -0,0 +1,21 @@
+prodiver = "google" {
+    credntials = ${{ secrets.CREDENTIALS }}
+    project = "My Project 73350"
+    region = "europe-west3-a"
+}
+
+resource "google_compute_instance" "vm_instance" {
+    name         = "gcptutorials-vm"
+    machine_type = "f1-micro"
+
+    boot_disk {
+    initialize_params {
+        image = "debian-cloud/debian-9"
+    }
+    }    
+    network_interface {       
+    network = "default"
+    access_config {
+    }
+    }
+}
